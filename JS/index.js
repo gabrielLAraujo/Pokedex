68c4e83f7298eb597ad5d66400d0673ab14c1b0e
Gabriel Araujo <gabriel.araujo@geopostenergy.com>
2022-07-29 22:47:35 -0300

    pagination and stats

--- JS/index.js
+++ JS/index.js
@@ -1,59 +1,120 @@
-let generations = ['i', 'ii', 'iii', 'iV', 'v', 'vi', 'vii', 'viii'];
-let pokemons = [];
+let page = 1;
+let offset = 0;
+let paginationDiv = document.getElementById('page_number_div');
 let searchInput = document.getElementById('searchInput');
-console.log(searchInput)
-for (let i = 0; i < 202; i++) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`, {
-        method: 'GET',
-    })
-        .then(response => response.json())
-        .then(pokemon => {
-            let pokedex = document.getElementById('pokedex'),
-                imageSrc = pokemon.sprites.other['official-artwork'].front_default,
-                moves = getFourRandomMoves(pokemon),
-                cardDiv = document.createElement('div'),
-                type = pokemon.types.map((el) => {
-                    return el.type.name
-                }).join(" | "),
-                html = '';
-            html = `<div class="pokemon_name">${pokemon.name}#${pokemon.id}</div>
-                <div>${type}</div>
-                <img class="pokemon_image" src="${imageSrc}" alt="${pokemon.name}"></img>
-                <div class="pokemon_moves">
-                `;
-            for (let j = 0; j < moves.length; j++) {
-                html += `<div>${moves[j]}</div>`
-            }
-            html += '</div>';
-            console.log(html)
-            cardDiv.innerHTML = html;
-            cardDiv.classList.add('card');
-            pokedex.append(cardDiv);
-            pokemon['htmlCard'] = cardDiv;
-            pokemons.push(pokemon);
+let pokedex = document.getElementById('pokedex');
+let nextPageBtn = document.getElementById('next_page');
+let backPageBtn = document.getElementById('back_page');
+window.addEventListener('load', (event) => {
+    // let generations = ['i', 'ii', 'iii', 'iV', 'v', 'vi', 'vii', 'viii'];
+    paginationDiv.textContent = page;
+    let pokemons = fetchPokemon(page)
+    console.log('The page has fully loaded');
+    initEvents()
+});
 
+
+const fetchPokemon = (page) => {
+    let pokemons = [];
+    console.log(offset, page)
+    for (let i = offset; i < 5 * page; i++) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`, {
+            method: 'GET',
         })
-}
-searchInput.addEventListener('input', (e) => {
-    for (let i = 0; i < pokemons.length; i++) {
-        if (pokemons[i].name.indexOf(e.target.value) == -1) {
-            pokemons[i].htmlCard.style.display = 'none';
-        } else {
-            pokemons[i].htmlCard.style.display = 'flex';
-        }
-    }
+            .then(response => response.json())
+            .then(pokemon => {
 
-})
+                let moves = getFourRandomMoves(pokemon),
+                    cardDiv = document.createElement('div'),
+                    html = createHtmlMoves(pokemon, moves);
+
+                cardDiv.innerHTML = html;
+                cardDiv.classList.add('card');
+                pokedex.append(cardDiv);
+                pokemon['htmlCard'] = cardDiv;
+                pokemons.push(pokemon);
+
+            })
+    }
+}
+const initEvents = () => {
+    searchInput.addEventListener('input', (e) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            if (pokemons[i].name.indexOf(e.target.value) == -1) {
+                pokemons[i].htmlCard.style.display = 'none';
+            } else {
+                pokemons[i].htmlCard.style.display = 'flex';
+            }
+        }
+
+    })
+    nextPageBtn.addEventListener('click', (e) => {
+        makePagination(true);
+    })
+    backPageBtn.addEventListener('click', (e) => {
+        if (page - 1 > 0)
+            makePagination(false)
+    })
+}
+const makePagination = (next) => {
+    if (next) {
+        page += 1;
+        offset += 5;
+    }
+    else {
+        page -= 1;
+        offset -= 5;
+    }
+    clearScreen();
+    fetchPokemon(page);
+    paginationDiv.textContent = page;
+}
+const createTypeText = (types) => {
+    return types.map((el) => {
+        return el.type.name
+    }).join(" | ");
+}
+const createHtmlStats = (stats) => {
+    let html = ""
+    stats.forEach(el => {
+        html += `<p>${el.stat.name}:  ${el.base_stat}</p>`
+    });
+    return html
+}
+const createHtmlMoves = (pokemon, moves) => {
+    let imageSrc = pokemon.sprites.other['official-artwork'].front_default,
+        type = createTypeText(pokemon.types),
+        html = '';
+    html = `<div class="pokemon_name">${pokemon.name}#${pokemon.id}</div>
+    <div>${type}</div>
+    <div class="pokemon_img_stats">
+    <div class="pokemon_img_div"><img class="pokemon_image" src="${imageSrc}" alt="${pokemon.name}"></img></div>
+    <div class="pokemon_stats">${createHtmlStats(pokemon.stats)}</div>
+    </div>
+    <div class="pokemon_moves">
+    `;
+    for (let j = 0; j < moves.length; j++) {
+        html += `<div class="pokemon_move">${moves[j]}</div>`
+    }
+    html += '</div>';
+    return html
+}
 const getFourRandomMoves = (pokemon) => {
     let moves = []
-    for (let i = 0; i < 4; i++) {
-        let position = Math.round(Math.random() * pokemon.moves.length - 1);
-        moves.push(pokemon.moves[position].move.name)
+    let size = pokemon.moves.length >= 4 ? 4 : pokemon.moves.length
+    for (let i = 0; i < size; i++) {
+        moves.push(pokemon.moves[i].move.name)
     }
+
     return moves;
 }
-// searchInput.onchange((text) => {
-//     console.log(text);
-// })
+const clearScreen = () => {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.remove();
+    });
+    pokemons = [];
+}
+
 
 
